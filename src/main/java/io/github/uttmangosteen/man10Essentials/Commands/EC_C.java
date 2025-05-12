5dc6a158cdd2251f822bc56ca95df604006c6d61
Underthetree <noisimportantpoint@gmail.com>
2025-05-13 03:15:01 +0900

    無駄なimport

--- src/main/java/io/github/uttmangosteen/man10Essentials/Commands/EC_C.java
+++ src/main/java/io/github/uttmangosteen/man10Essentials/Commands/EC_C.java
@@ -5,7 +5,6 @@ import org.bukkit.Sound;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 public class EC_C implements CommandExecutor {
