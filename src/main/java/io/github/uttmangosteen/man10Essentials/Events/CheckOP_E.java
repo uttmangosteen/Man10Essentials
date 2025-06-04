9466337024241277b4c39008a6f29e2a9b3f0a07
Underthetree <noisimportantpoint@gmail.com>
2025-06-05 04:41:23 +0900

    opCheck機能追加

--- src/main/java/io/github/uttmangosteen/man10Essentials/Events/CheckOP_E.java
+++ src/main/java/io/github/uttmangosteen/man10Essentials/Events/CheckOP_E.java
@@ -1,5 +1,6 @@
 package io.github.uttmangosteen.man10Essentials.Events;
 
+import io.github.uttmangosteen.man10Essentials.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,7 +11,7 @@ public class CheckOP_E implements Listener {
     @EventHandler
     public void onLogin(PlayerLoginEvent e) {
         Player p = e.getPlayer();
-        if(!p.isOp() || p.hasPermission("group.op")) return;
+        if(!p.isOp() || p.hasPermission("group.gm")) return;
         p.setOp(false);
         Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "report 不正なOPを検知したため権限を剝奪しました 対象者:" + p.getName());
         Bukkit.getLogger().info("不正なOPを検知したため権限を剝奪しました 対象者:" + p.getName());
